db5fc75a34e56fc0b6567532e1ee049e962658bb
Jensen Kaplan <jensenkaplan@gmail.com>
2021-11-18 11:45:21 -0500

    Only leaving one instance of JT in top directory. Scripts will be upated to pullJensenTools from there.

---
+++ packaging_tutorial/src/sub/testing.py
@@ -0,0 +1,12 @@
+import sys 
+sys.path.append('..')
+import example as e
+import ex2 as e2
+
+print(e.add_one(5))
+e2.printHi()
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
