6c5631c193f2e436df0780c94755e932dba546cb
Jensen Kaplan <jensenkaplan@gmail.com>
2021-11-26 02:56:33 -0500

    Updated to wrok with new JT functions. Still need to cleanup and comment.

--- MvsH/M_vs_H.py
+++ MvsH/M_vs_H.py
@@ -13,8 +13,9 @@ import sys
 sys.path.append('..')
 from JensenTools import *
 
+who = 'Arun'
 comp = 'Sr2PrO4'
-dataDir = getSaveDir('m', comp = comp, dataType = 'MH')
+MHDir = getSaveDir('m', comp = comp, dataType = 'MH')
 
 
 # # Load the M vs H data files
@@ -28,13 +29,13 @@ dataDir = getSaveDir('m', comp = comp, dataType = 'MH')
 
 
 runs = [] #A list of all the data file names
-for i in os.listdir(dataDir):
+for i in os.listdir(MHDir):
     if i.endswith('.DAT'): #This was a safeguard against a situation arising at an earlier implementation of my code.
         runs.append(i)
 
 molweight = 380.15
-mass = getMass(runs[0])
-T = getTemp(runs[0])
+mass = getMass(runs[0], who = who)
+T = getTemp(runs[0], who = who)
 
 
 # # Order the files by temperature
@@ -45,29 +46,29 @@ T = getTemp(runs[0])
 
 temp = [] #The newly sorted list
 for i in runs:
-    temp.append(getTemp(i)) #this creates a list of just temperatures as read by the filename
+    temp.append(getTemp(i, who = who)) #this creates a list of just temperatures as read by the filename
    
 temp = np.argsort([int(i) for i in temp]) #Sort by temperature
 runs = [runs[i] for i in temp]
 
 
-# # Plot all runs and store data in dictionary 'data'
-# ## H = Applied magnetic field, M = Magnetization, Err = Error on the Magnetization measurement
 
-# In[76]:
-
-
-#This dictionary easily allows me to access the data from a specific run
+# This dictionary easily allows me to access the data from a specific run
 ## {'Temperature': [H,M,Err]} 
-data = {}
+MHdata = {}
 plt.figure()
 for i in runs:
-    H, M, Err, name = getData(i,dataDir,who = 'Arun')
+    M, H, Err, mass, T = getData(i,MHDir,who = who, dataType = 'MH')
     M = emuToBohr(M,mass,molweight)
     H = oeToTesla(H)
     Err = emuToBohr(Err,mass,molweight)
-    data[name] = [H,M,Err]
-    plt.errorbar(H,M, yerr = Err, label = name)
+    MHdata[T] = [M,H,Err,mass,i]
+    plt.errorbar(H,M, yerr = Err, label = T)
+
+# MHData = {}
+# for i in runs:
+#     M, H, Err, mass, T = getData(i,MHDir,who = who, dataType = 'MH')
+#     MHdata[T] = [M,H,Err,mass,i]
 
 plt.title(comp)
 plt.ylabel('Moment (\N{GREEK SMALL LETTER MU}B)')
@@ -76,23 +77,12 @@ plt.legend()
 plt.show()
 
 
-# # Choose a run to fit for saturation magnetization.
-# ## Usually the lowest temperature run
-
-# In[77]:
-
-
 temp = '20K'
-curRun = data[temp] #loading the data from my current run
+curRun = MHdata[temp] #loading the data from my current run
 H = curRun[0]
 M = curRun[1]
 Err = curRun[2]
 
-
-# # Choose your field range to fit over.
-# ## The saturation magnetization is a linear fit and should be fitted within a reasonable magnetic field range that makes sense for the system.
-
-# In[78]:
 
 
 fieldRange = [0,14]
@@ -106,29 +96,15 @@ for i in range(len(curRun[0])):
         newErr.append(Err[i])
 
 
-# # Create the linear model and fit to the data over the desired range.
-
-# In[79]:
-
 
 linModel = LinearModel()
 params = linModel.guess(newM, x = newH)
 fitted = linModel.fit(newM, x = newH, weights = newErr)
 
-
-# # Create a full length line using the fitted parameters.
-
-# In[80]:
-
-
 MLine = []
 for i in H:
     MLine.append(fitted.params['slope'].value*i + fitted.params['intercept'].value)
 
-
-# # Plot the data and the fit
-
-# In[81]:
 
 
 plt.plot(H,M, label = temp)
