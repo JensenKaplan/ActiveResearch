7b9f8a902b08ff4d9dea3e74ed0225637de2bd51
Jensen Kaplan <jensenkaplan@gmail.com>
2021-11-26 02:59:23 -0500

    Updated to work with new JT functions. Need to clean and comment.

--- MvsH/MvsH.py
+++ MvsH/MvsH.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[72]:
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -7,10 +13,9 @@ import sys
 sys.path.append('..')
 from JensenTools import *
 
-comp = 'Sr2PrO4'
 who = 'Arun'
-dataType = 'MH'
-dataDir = getSaveDir('m', comp = comp, dataType = 'MH')
+comp = 'Sr2PrO4'
+MHDir = getSaveDir('m', comp = comp, dataType = 'MH')
 
 
 # # Load the M vs H data files
@@ -24,7 +29,7 @@ dataDir = getSaveDir('m', comp = comp, dataType = 'MH')
 
 
 runs = [] #A list of all the data file names
-for i in os.listdir(dataDir):
+for i in os.listdir(MHDir):
     if i.endswith('.DAT'): #This was a safeguard against a situation arising at an earlier implementation of my code.
         runs.append(i)
 
@@ -41,29 +46,29 @@ T = getTemp(runs[0], who = who)
 
 temp = [] #The newly sorted list
 for i in runs:
-    temp.append(getTemp(i,who = who)) #this creates a list of just temperatures as read by the filename
+    temp.append(getTemp(i, who = who)) #this creates a list of just temperatures as read by the filename
    
 temp = np.argsort([int(i) for i in temp]) #Sort by temperature
 runs = [runs[i] for i in temp]
 
 
-# # Plot all runs and store data in dictionary 'data'
-# ## H = Applied magnetic field, M = Magnetization, Err = Error on the Magnetization measurement
 
-# In[76]:
-
-
-#This dictionary easily allows me to access the data from a specific run
+# This dictionary easily allows me to access the data from a specific run
 ## {'Temperature': [H,M,Err]} 
-data = {}
+MHdata = {}
 plt.figure()
 for i in runs:
-    H, M, Err, name = getData(i,dataDir, who = who, dataType = dataType)
-    MBohr = emuToBohr(M,mass,molweight)
-    HTes = oeToTesla(H)
-    ErrBohr = emuToBohr(Err,mass,molweight)
-    data[name] = [H,M,Err]
-    plt.errorbar(H,M, yerr = Err, label = name)
+    M, H, Err, mass, T = getData(i,MHDir,who = who, dataType = 'MH')
+    M = emuToBohr(M,mass,molweight)
+    H = oeToTesla(H)
+    Err = emuToBohr(Err,mass,molweight)
+    MHdata[T] = [M,H,Err,mass,i]
+    plt.errorbar(H,M, yerr = Err, label = T)
+
+# MHData = {}
+# for i in runs:
+#     M, H, Err, mass, T = getData(i,MHDir,who = who, dataType = 'MH')
+#     MHdata[T] = [M,H,Err,mass,i]
 
 plt.title(comp)
 plt.ylabel('Moment (\N{GREEK SMALL LETTER MU}B)')
@@ -72,70 +77,40 @@ plt.legend()
 plt.show()
 
 
-# # Choose a run to fit for saturation magnetization.
-# ## Usually the lowest temperature run
-
-# In[77]:
-
-# print(data.keys())
 temp = '20K'
-curRun = data[temp] #loading the data from my current run
+curRun = MHdata[temp] #loading the data from my current run
 H = curRun[0]
 M = curRun[1]
 Err = curRun[2]
 
 
 
-M = normalizeSpin(M,mass,molweight)
-Err = normalizeSpin(Err,mass,molweight)
-HTes = oeToTesla(H)
-
-# # Choose your field range to fit over.
-# ## The saturation magnetization is a linear fit and should be fitted within a reasonable magnetic field range that makes sense for the system.
-
-# In[78]:
-
-
-fieldRange = [12,14]
+fieldRange = [0,14]
 newH = []
 newM = []
 newErr = []
 for i in range(len(curRun[0])):
-    if (HTes[i] >= fieldRange[0] and HTes[i] <= fieldRange[1]):
+    if (H[i] >= fieldRange[0] and H[i] <= fieldRange[1]):
         newH.append(H[i])
         newM.append(M[i])
         newErr.append(Err[i])
 
-
-# # Create the linear model and fit to the data over the desired range.
-
-# In[79]:
 
 
 linModel = LinearModel()
 params = linModel.guess(newM, x = newH)
 fitted = linModel.fit(newM, x = newH, weights = newErr)
 
-
-# # Create a full length line using the fitted parameters.
-
-# In[80]:
-
-
 MLine = []
 for i in H:
     MLine.append(fitted.params['slope'].value*i + fitted.params['intercept'].value)
 
 
-# # Plot the data and the fit
-
-# In[81]:
-
 
 plt.plot(H,M, label = temp)
-# plt.plot(H,MLine, linestyle = '--', label = 'Fitted')
-plt.xlabel('Field (oe)')
-plt.ylabel('Magnetization (emu mol^-1)')
+plt.plot(H,MLine, linestyle = '--', label = 'Fitted')
+plt.xlabel('Field (T)')
+plt.ylabel('Magnetization (bohr magneton)')
 plt.legend()
 plt.title(comp)
 plt.show()
