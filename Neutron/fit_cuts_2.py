a0f9e2c2fcf7ddfb60f8e18d7609ae37c5321b6f
Jensen Kaplan <jensenkaplan@lawn-143-215-50-43.lawn.gatech.edu>
2022-06-21 12:42:53 -0400

    pulled recent version from Sr2PrO4

--- Neutron/fit_cuts_2.py
+++ Neutron/fit_cuts_2.py
@@ -6,10 +6,20 @@ from lmfit.models import LinearModel, LorentzianModel, GaussianModel, VoigtModel
 from matplotlib import rcParams
 from matplotlib import patches
 
+
+def paramPrint(fittedparams):
+    print()
+    for i in fittedparams:
+        # print(i, ' = ', i.value)
+        print(i, ' = ',fittedparams[i].value )
+
 #####################################################################################################################################################################
+fig = plt.figure(figsize = (10,10))
+gs = fig.add_gridspec(1, 1)
+ax1 = plt.subplot(gs[0])
 rcParams['font.family'] = 'sans-serif'
 rcParams['font.sans-serif'] = ['Arial']
-rcParams.update({'font.size': 28})
+rcParams.update({'font.size': 15})
 rcParams['font.weight'] = 'bold'
 rcParams['axes.linewidth'] = 4
 rcParams['xtick.direction'] = 'out'
@@ -34,11 +44,14 @@ rcParams['legend.fontsize'] = 18
 def labelMakerFinal(run):
     x = run.split('.')[0].split('_')
     return (x[0] + ' Ei = ' + x[1].replace("p",".") + " meV, T = " + x[2].replace("p",".") + " K" )
+def plotSaver(run):
+    x = run.split('.')[0].split('_')
+    return (x[0] + '_Ei_' + x[1] + "_meV.png")
 def labelMaker(run, energyIntegration, qIntegration):
     x = run.split('.')[0].split('_')
     return (x[0] + ' Ei = ' + x[1].replace("p",".") + " meV, T = " + x[2].replace("p",".") + " K" + " -- " + energyIntegration + " -- " + qIntegration)
 
-runDir = '/Users/jensenkaplan/Dropbox (GaTech)/Jensen/IE_runs/'
+runDir = '/Users/jensenkaplan/Dropbox (GaTech)/Jensen/Sr2PrO4/IE_runs/'
 # runDir = '/Users/jensenkaplan/Dropbox (GaTech)/Jensen/March Meeting/slice/'
 dirList = os.listdir(runDir)
 runList = []
@@ -52,7 +65,7 @@ for i in range(len(runList)):
 
 # Ei = 700meV SrPr
 # nonmagrun = runList[14]
-# magrun = runList[4]
+# magrun = runList[12]
 # Ei = 500 meV SrPr
 # nonmagrun = runList[2]
 # magrun = runList[9]
@@ -75,7 +88,13 @@ emag = datamag[:,0]
 signalmag = datamag[:,1]
 errormag = datamag[:,2]
 
+#SrPr 700mev
+tr = [225,500]
+#SrPr 500mev
 tr = [0,1000]
+#LiPr 500meV
+tr = [0,1000]
+
 
 newE = []
 newSignalMag = []
@@ -87,23 +106,30 @@ for i in range(len(emag)):
         newE.append(emag[i])
         newSignalMag.append(signalmag[i])
         newErrorMag.append(errormag[i])
+
+emag = np.array(newE)
+signalmag = np.array(newSignalMag)
+errormag = np.array(newErrorMag)
+
+newE = []
+for i in range(len(enonmag)):
+    if (enonmag[i] >= tr[0] and enonmag[i]<= tr[1]):
+        newE.append(enonmag[i])
         newSignalNonMag.append(signalnonmag[i])
         newErrorNonMag.append(errornonmag[i])
-        
-        
-enonmag = newE
-emag = newE
-signalnonmag = newSignalNonMag
-signalmag = newSignalMag
-errornonmag = newErrorNonMag
-errormag = newErrorMag
+                
+enonmag = np.array(newE)
+signalnonmag = np.array(newSignalNonMag)
+errornonmag = np.array(newErrorNonMag)
 
+# plt.plot(enonmag,signalnonmag,emag,signalmag)
+# plt.show()
 # SrPr
 #####################################################################################################################################################################
 # bkg = PolynomialModel(prefix = 'bg_', degree = 3)
 # params = bkg.guess(signalmag,x = emag)
 # bkgparams = bkg.make_params()
-# bkgparams['bg_c0'].set(value = 3.5e-6, vary = True)
+# bkgparams['bg_c0'].set(value = 3.5e-6, vary = True )
 # # bkgparams['bg_c1'].set(value =  -2.526e-07  )
 # # bkgparams['bg_c2'].set(value = 2.926e-10 )
 
@@ -114,28 +140,28 @@ errormag = newErrorMag
 
 # myModel = bkg + g1 + g2
 
-# # # Ei = 700meV
-# params['g1_center'].set(value = 334)
+# # Ei = 700meV
+# params['g1_center'].set(value = 334.5)
 # params['g1_sigma'].set(value = 5)
-# params['g2_center'].set(value = 388)
-# params['g2_sigma'].set(value = 5)
+# params['g2_center'].set(value = 388.8, vary = True)
+# params['g2_sigma'].set(value = 3)
 # params['g2_amplitude'].set(value = .15, min = 0)
 
 
-# # ## Ei = 500 meV
-# g3 = GaussianModel(prefix = 'g3_')
-# params.update(g3.make_params())
-# params['g1_center'].set(value = 166)
-# params['g1_sigma'].set(value = 7)
-# params['g2_center'].set(value = 220)
-# params['g2_sigma'].set(value = 2)
-# params['g3_center'].set(value = 334)
-# params['g3_sigma'].set(value = 5)
-# myModel = bkg + g1 + g2 + g3
-####################################################################################################################################################################
+# # # # # ## Ei = 500 meV
+# # g3 = GaussianModel(prefix = 'g3_')
+# # params.update(g3.make_params())
+# # params['g1_center'].set(value = 168)
+# # params['g1_sigma'].set(value = 7)
+# # params['g2_center'].set(value = 220)
+# # params['g2_sigma'].set(value = 2)
+# # params['g3_center'].set(value = 330)
+# # params['g3_sigma'].set(value = 5)
+# # myModel = bkg + g1 + g2 + g3
+# ####################################################################################################################################################################
 
-# LiPr
-#####################################################################################################################################################################
+#LiPr
+# #####################################################################################################################################################################
 bkg = PolynomialModel(prefix = 'bg_', degree = 3)
 params = bkg.guess(signalmag,x = emag)
 bkgparams = bkg.make_params()
@@ -144,41 +170,28 @@ bkgparams['bg_c0'].set(value = 3.5e-6, vary = True )
 # bkgparams['bg_c2'].set(value = 2.926e-10 )
 
 g1 = GaussianModel(prefix = 'g1_')
-# g2 = GaussianModel(prefix = 'g2_')
+# # g2 = GaussianModel(prefix = 'g2_')
 params.update(g1.make_params())
-# params.update(g2.make_params())
-
-myModel = bkg + g1
-
-# # Ei = 700meV
-# # params['g1_center'].set(value = 330)
-# # params['g1_sigma'].set(value = 5)
-# # params['g2_center'].set(value = 385)
-# # params['g2_sigma'].set(value = 5)
-# # params['g2_amplitude'].set(value = .15, min = 0)
+# # params.update(g2.make_params())
 
 
-# # ## Ei = 500 meV
-# g3 = GaussianModel(prefix = 'g3_')
-# params.update(g3.make_params())
+# ## Ei = 500 meV
 params['g1_center'].set(value = 270)
 params['g1_sigma'].set(value = 7)
-# params['g2_center'].set(value = 220)
-# params['g2_sigma'].set(value = 2)
-# params['g3_center'].set(value = 330)
-# params['g3_sigma'].set(value = 5)
 myModel = bkg + g1
 #####################################################################################################################################################################
 
+print('\n\n\n')
 # bkgresult = bkg.fit(signalmag,bkgparams, x = emag, weights = errormag)
 result = myModel.fit(signalmag,params, x = emag)
 result.params.pretty_print()
+paramPrint(result.params)
 # bkgresult.params.pretty_print()
 
-comps = result.eval_components()
+# comps = result.eval_components()
 
-print(comps.keys())
-plt.figure()
+# print(comps.keys())
+# plt.figure()
 # plt.errorbar(emag, signalmag, yerr = errormag, fmt = 'o', linestyle = 'none',alpha=.5,label = labelMakerFinal(magrun))
 # plt.errorbar(emag,signalnonmag, yerr = errornonmag, fmt = 'o', linestyle = 'none',alpha=.5,label = labelMakerFinal(nonmagmagrun))
 plt.errorbar(emag, signalmag, linestyle = '--',color='magenta',marker = 'o',markersize = 5,linewidth = 4, label = labelMakerFinal(magrun))
@@ -189,9 +202,73 @@ plt.xlabel('Energy (meV)', fontweight = 'bold')
 plt.ylabel('Intensity (Arb. Units)', fontweight = 'bold')
 # plt.plot(emag, result.init_fit)
 # plt.plot(emag,comps['bg_'],emag,comps['g1_'],emag,comps['g2_'])
-plt.legend(fontsize = 30)
+plt.legend()
+plt.savefig(runDir + 'pics/' +plotSaver(magrun))
 plt.show()
-print(labelMakerFinal(nonmagrun))
-print(labelMakerFinal(magrun))
+
+# print(labelMakerFinal(nonmagrun))
+# print(labelMakerFinal(magrun))
 # print(result.params['g1_amplitude']/result.params['g3_amplitude'])
 # print(result.params['g2_amplitude']/result.params['g1_amplitude'])
+
+#LiPr For Paper
+#####################################################################################################################################################################
+bg_c0  =  0.00018505104444559132
+bg_c1  =  -1.5019691570118256e-06
+bg_c2  =  4.323248077115866e-09
+bg_c3  =  -4.257487047838987e-12
+g1_amplitude  =  0.0002769855396335671
+g1_center  =  274.2196764501529
+g1_sigma  =  9.101311129042994
+g1_fwhm  =  21.431949472893024
+g1_height  =  1.214124500101291e-05
+#####################################################################################################################################################################
+
+#SrPr For Paper
+#####################################################################################################################################################################
+#500 meV
+bg_c0  =  0.00018455782658675292
+bg_c1  =  -1.4718141769515983e-06
+bg_c2  =  4.1180944701962e-09
+bg_c3  =  -3.91981609197623e-12
+g1_amplitude  =  0.00010025238254331347
+g1_center  =  167.86803392064175
+g1_sigma  =  8.28491499033998
+g1_fwhm  =  19.509483517552393
+g1_height  =  4.827438316378923e-06
+g2_amplitude  =  0.00021057360331221768
+g2_center  =  227.71534226725424
+g2_sigma  =  14.189659743957016
+g2_fwhm  =  33.414094558264864
+g2_height  =  5.920277098993856e-06
+g3_amplitude  =  0.00011474894377347192
+g3_center  =  334.8342674591908
+g3_sigma  =  6.138000561491445
+g3_fwhm  =  14.453886482211287
+g3_height  =  7.4581628158789436e-06
+
+#700 meV
+bg_c0  =  0.00016963603713744712
+bg_c1  =  -1.2593727621092305e-06
+bg_c2  =  3.101749296944818e-09
+bg_c3  =  -2.4733272591912303e-12
+g1_amplitude  =  0.00022882035627084753
+g1_center  =  335.3525716811439
+g1_sigma  =  13.90613734716213
+g1_fwhm  =  32.74645034784433
+g1_height  =  6.564448267594624e-06
+g2_amplitude  =  5.277865776909252e-05
+g2_center  =  382.2838548009725
+g2_sigma  =  8.998765891237555
+g2_fwhm  =  21.190473896004022
+g2_height  =  2.3398363037555323e-06
+#####################################################################################################################################################################
+
+
+
+# # # Ei = 700meV
+# # params['g1_center'].set(value = 330)
+# # params['g1_sigma'].set(value = 5)
+# # params['g2_center'].set(value = 385)
+# # params['g2_sigma'].set(value = 5)
+# # params['g2_amplitude'].set(value = .15, min = 0)
--- Neutron/make_lineshape.py
+++ Neutron/make_lineshape.py
@@ -1,6 +1,36 @@
 import sys
 sys.path.append('..')
 from JensenTools import *
+from matplotlib import rcParams
+from matplotlib import patches
+import pandas as pd
+
+#####################################################################################################################################################################
+fig = plt.figure(figsize = (10,10))
+gs = fig.add_gridspec(1, 1)
+ax1 = plt.subplot(gs[0])
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Arial']
+rcParams.update({'font.size': 15})
+rcParams['font.weight'] = 'bold'
+rcParams['axes.linewidth'] = 4
+rcParams['xtick.direction'] = 'out'
+rcParams['ytick.direction'] = 'out'
+rcParams['xtick.top'] = False
+rcParams['ytick.right'] = False
+rcParams['xtick.major.size'] = 12.5
+rcParams['ytick.major.size'] = 12.5
+rcParams['xtick.minor.size'] = 7.5
+rcParams['ytick.minor.size'] = 7.5
+rcParams['xtick.major.width'] = 3
+rcParams['ytick.major.width'] = 3
+rcParams['xtick.minor.width'] = 3
+rcParams['ytick.minor.width'] = 3
+rcParams['xtick.minor.visible'] = True
+rcParams['ytick.minor.visible'] = True
+rcParams['legend.frameon'] = False
+rcParams['legend.fontsize'] = 18
+#####################################################################################################################################################################
 
 def gaussian(mu,sigma,amp,x):
     g = []
@@ -9,7 +39,7 @@ def gaussian(mu,sigma,amp,x):
     return np.array(g)
 
 
-x = np.linspace(30,500,1000) # Generate energy X-axis. 0-450 should do since highest peak is ~380
+
 
 # #Create the 3 gaussians with parameters determine by LMFIT
 # g1 = gaussian(mu = 168.22215251994697,sigma = 4.095279662587834,amp = .0002701750011412199, x = x) #Ei = 300meV, T = 4.88K
@@ -26,35 +56,96 @@ x = np.linspace(30,500,1000) # Generate energy X-axis. 0-450 should do since hig
 # g3 = gaussian(mu = 332.8, sigma =  23.93, amp = .0005951, x = x) #Ei = 700meV, T = 4.76K
 # g4 = gaussian(mu = 388, sigma =  24.48, amp = .000292, x = x) #Ei = 700meV, T = 4.76K
 
+# SrPr
+# #####################################################################################################################################################################
+# g1amp = .000324
+# g2amp = 8.938e-5
+# g22amp = .0001005 
+# g3amp = 7.9943e-6
+# g1g2 = 0.7057918532469233
+# g3g2 = .25
+
+# print('g1amp/g2amp = ', g1g2 )
+# print('g3amp/g2amp = ', g3g2 )
+
+# sigma = 3
+# width = sigma/2.35
+# x = np.linspace(30,500,1000) # Generate energy X-axis. 0-450 should do since highest peak is ~380
 
 
-g1amp = .000324
-g2amp = 8.938e-5
-g22amp = .0001005
-g3amp = 7.9943e-6
-g1g2 = 0.7057918532469233
-g3g2 = .25
-print('g1amp/g2amp = ', g1g2 )
-print('g3amp/g2amp = ', g3g2 )
+# g1 = gaussian(mu = 166.2, sigma = width, amp = g1g2, x = x) #Ei = 500meV, T = 4.88K
+# g2 = gaussian(mu = 334.5, sigma =  width, amp = 1, x = x) #Ei = 500meV, T = 4.88K
+# # g22 = gaussian(mu = 332.8, sigma =  3, amp = .0005951, x = x) #Ei = 700meV, T = 4.76K
+# g3 = gaussian(mu = 388.8, sigma =  width, amp = g3g2, x = x) #Ei = 700meV, T = 4.76K
+
+
+
+# curve = np.array(g1) + np.array(g2)  + np.array(g3)
+#####################################################################################################################################################################
+
+#LiPr 274 mode
+#####################################################################################################################################################################
+# g1_amplitude = 0.0003207 
+# g1_center = 274.3 
+# g1_fwhm  = 23.41 
+# g1_height = 1.287e-05 
+# g1_sigma  = 9.941    
+#####################################################################################################################################################################
+
+#SRPR FOR PAPER
+#####################################################################################################################################################################
+# 500 meV
+g1_amplitude  =  0.00010025238254331347
+g1_center  =  167.86803392064175
+g1_sigma  =  8.28491499033998
+g1_fwhm  =  19.509483517552393
+g1_height  =  4.827438316378923e-06
+g2_amplitude  =  0.00011474894377347192
+g2_center  =  334.8342674591908
+g2_sigma  =  6.138000561491445
+g2_fwhm  =  14.453886482211287
+g2_height  =  7.4581628158789436e-06
+
+
+#700 meV
+g2_amplitude  =  0.00022882035627084753
+g2_center  =  335.3525716811439
+g2_sigma  =  13.90613734716213
+g2_fwhm  =  32.74645034784433
+g2_height  =  6.564448267594624e-06
+g3_amplitude  =  5.277865776909252e-05
+g3_center  =  382.2838548009725
+g3_sigma  =  8.998765891237555
+g3_fwhm  =  21.190473896004022
+g3_height  =  2.3398363037555323e-06
 
 sigma = 6
 width = sigma/2.35
+x = np.linspace(30,700,1000) # Generate energy X-axis. 0-450 should do since highest peak is ~380
 
-g1 = gaussian(mu = 166.2, sigma = width, amp = g1g2, x = x) #Ei = 500meV, T = 4.88K
-g2 = gaussian(mu = 334.5, sigma =  width, amp = 1, x = x) #Ei = 500meV, T = 4.88K
-# g22 = gaussian(mu = 332.8, sigma =  3, amp = .0005951, x = x) #Ei = 700meV, T = 4.76K
-g3 = gaussian(mu = 388.8, sigma =  width, amp = g3g2, x = x) #Ei = 700meV, T = 4.76K
+# g1_cen = 166.2
+# g2_cen = 334.5
+# g3_cen = 388.8
+
+g1 = gaussian(mu = g1_center, sigma = width, amp = g1_amplitude/g2_amplitude, x = x) # Ei = 500meV, T = 4.88K
+g2 = gaussian(mu = g2_center, sigma = width, amp = 1, x = x) # From Optics
+g3 = gaussian(mu = g3_center, sigma = width, amp = g3_amplitude/g2_amplitude, x = x) # From Optics
+
+curve = np.array(g1) + np.array(g2) + np.array(g3)
+#####################################################################################################################################################################
 
 
-curve = np.array(g1) + np.array(g2)  + np.array(g3)
 
 
-lineErr = .003*np.ones(len(curve))
-# lineErr = .01*curve
+lineErr = .00005*np.ones(len(curve))
+# lineErr = 1*(curve)
 
 
-plt.figure()
+# plt.figure()
 plt.errorbar(x,curve,yerr = lineErr)
+plt.xlabel('Energy (meV)')
+plt.ylabel('Intensity (a.u.)')
+plt.savefig('/Users/jensenkaplan/Dropbox (GaTech)/Jensen/Sr2PrO4/Sr2PrO4_Produced_Gaussians_FINALPAPER.png')
 plt.show()
 
 # np.savetxt('/Users/jensenkaplan/Dropbox (GaTech)/Jensen/Sr2PrO4/Sr2PrO4_scaled.ascii',np.transpose([x,curve]), header = 'Energy (meV), Intensity (arb. units)')
@@ -63,8 +154,7 @@ df['Energy'] = x
 df['Intensity'] = curve
 df['Error'] = lineErr
 
-
-df.to_csv('/Users/jensenkaplan/Dropbox (GaTech)/Jensen/Sr2PrO4/Sr2PrO4_gaussians.csv')
+df.to_csv('/Users/jensenkaplan/Dropbox (GaTech)/Jensen/Sr2PrO4/Sr2PrO4_Produced_Gaussians_FINALPAPER.csv')
 
 
 # f ='/Users/jensenkaplan/Dropbox (GaTech)/Jensen/Sr2PrO4/Sr2PrO4_scaled.csv' # We need to re-open the file
